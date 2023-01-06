29007c1dbd0d8f0074dc7c22bab7e28d4e0a87e8
Mohammad Mustafa <mustafa@sherpa-tech.com>
2023-01-06 21:57:24 +0545

    statefull widget explained

--- lib/main.dart
+++ lib/main.dart
@@ -13,185 +13,229 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CreateAccount extends StatelessWidget {
+class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
 
   @override
+  State<CreateAccount> createState() {
+    print("create state called");
+    return _CreateAccountState();
+  }
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  bool rememberMe = false;
+  @override
+  void didChangeDependencies() {
+    print("didChangeDependencies called");
+    super.didChangeDependencies();
+  }
+
+  @override
+  void initState() {
+    print("initstate called");
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant CreateAccount oldWidget) {
+    print("did update widget called");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    print("deactivate called");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print("dispose called");
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build called");
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    children: const [
-                      Text(
-                        "5:19",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.black,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 76,
-                    width: 76,
-                    color: const Color(0xffB5E742),
-                  ),
-                  Container(
-                    height: 76,
-                    width: 76,
-                    color: const Color(0xffe1f5b3),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 76,
-                    width: 76,
-                    color: const Color(0xffe1f5b3),
-                  ),
-                  Container(
-                    height: 76,
-                    width: 76,
-                    color: const Color(0xfff3fbe1),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Text(
-                "Create New Account",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Align(
+                alignment: Alignment.topLeft,
                 child: Column(
-                  children: [
-                    TextField(
-                      cursorColor: Colors.black,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: const TextStyle(
-                          color: Color(0xff000000),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        suffixIcon: const Icon(Icons.circle),
-                        suffixIconColor: Colors.blue,
+                  children: const [
+                    Text(
+                      "5:19",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    const SizedBox(
-                      height: 10,
+                    SizedBox(
+                      height: 25,
                     ),
-                    TextField(
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: const TextStyle(
-                          color: Color(0xff000000),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        suffixIcon: const Icon(Icons.circle),
-                        suffixIconColor: Colors.blue,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      cursorColor: Colors.black,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        labelText: "Recent Password",
-                        labelStyle: const TextStyle(
-                          color: Color(0xff000000),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        suffixIcon: const Icon(Icons.circle),
-                        suffixIconColor: Colors.blue,
-                      ),
+                    Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
                     ),
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Text(
-                "By creating account you agree\nto our terms of conditions",
-                style: TextStyle(
-                  color: Color(0xffD9D9D9),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 76,
+                  width: 76,
+                  color: const Color(0xffB5E742),
                 ),
+                Container(
+                  height: 76,
+                  width: 76,
+                  color: const Color(0xffe1f5b3),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 76,
+                  width: 76,
+                  color: const Color(0xffe1f5b3),
+                ),
+                Container(
+                  height: 76,
+                  width: 76,
+                  color: const Color(0xfff3fbe1),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Create New Account",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(
-                height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.circle_outlined,
-                    color: Colors.blue,
+              child: Column(
+                children: [
+                  TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: const TextStyle(
+                        color: Color(0xff000000),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      suffixIcon: const Icon(Icons.circle),
+                      suffixIconColor: Colors.blue,
+                    ),
                   ),
-                  Text(
-                    "remember me",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: const TextStyle(
+                        color: Color(0xff000000),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      suffixIcon: const Icon(Icons.circle),
+                      suffixIconColor: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Recent Password",
+                      labelStyle: const TextStyle(
+                        color: Color(0xff000000),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      suffixIcon: const Icon(Icons.circle),
+                      suffixIconColor: Colors.blue,
                     ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 10,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "By creating account you agree\nto our terms of conditions",
+              style: TextStyle(
+                color: Color(0xffD9D9D9),
               ),
-              Column(children: [
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                  value: rememberMe,
+                  onChanged: (value) {
+                    setState(() {
+                      rememberMe = !rememberMe;
+                    });
+                    print(rememberMe);
+                  },
+                ),
+                const Text(
+                  "remember me",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
                 InkWell(
                   onTap: () {},
                   child: Container(
@@ -235,10 +279,23 @@ class CreateAccount extends StatelessWidget {
                     ),
                   ),
                 ),
-              ]),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("This is dashboard page"),
       ),
     );
   }
